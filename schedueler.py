a8e727c417f6fd3eef408c1e7a8cda4364177c45
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2013-04-16 21:13:41 +0200

    Finished fairshare

--- schedueler.py
+++ schedueler.py
@@ -52,7 +52,6 @@ def lottery(processes=[], priorities=[]):
       name, user, starttime, remaining =\
         filter(lambda (a,b,c,d): a == name, processes)[0]
       remaining -= 2
-      if rem
       remaining = remaining >= 0 and remaining or 0
       processes = filter(lambda (a,b,c,d): a != name, processes)
       if remaining > 0:
@@ -61,7 +60,7 @@ def lottery(processes=[], priorities=[]):
       else:
         box = filter(lambda (a,b): a != name, box)
         next_quantum = quantums + 1
-      print "Frome time %i to %i: %s remaining time: %i" % (quantums,
+      print "From time %i to %i: %s remaining time: %i" % (quantums,
       next_quantum, name, remaining)
       quantums = next_quantum
       if remaining <= 0:
@@ -72,14 +71,87 @@ def lottery(processes=[], priorities=[]):
       print "No processes are running at time %i" % quantums
       quantums += 1
 
+def fair_share(processes=[], priorities=[]):
+  user_queues = []
+  ballot = []
+  for (owner, priority) in priorities:
+    queue = filter(lambda (a,b,c,d): b == owner, processes)
+    # queue = map(lambda (a,b,c,d): (a,c), queue)
+    user_queues.append((owner, queue))
+    ballot += [owner] * priority
+  random.shuffle(ballot)
+
+  print ballot, user_queues
+
+  quantums = 0
+  finished_processes = 0
+  while True:
+    misses = 0
+    no_processes = False
+
+    while True:
+      owner = ballot.pop(0)
+      user_queue = filter(lambda (a,b): a == owner, user_queues)[0]
+      if user_queue:
+        owner, owned_processes = user_queue
+        available_processes = filter(lambda (a,b,c,d): c <= quantums,
+        owned_processes)
+      else:
+        available_processes = []
+
+      ballot.append(owner)
+      if available_processes:
+        name, owner, starttime, remaining = available_processes.pop(0)
+        owned_processes = filter(lambda (a,b,c,d): a != name, owned_processes)
+        user_queues = filter(lambda (a,b): a != owner, user_queues)
+        break
+      else:
+        misses += 1
+        if misses == len(ballot):
+          no_processes = True
+          break
+
+    if no_processes:
+      print "No processes running at time %i" % quantums
+      quantums += 1
+      if quantums >10:
+        break
+      continue
+    else:
+      if remaining >= 2:
+        remaining -= 2
+        increment = 2
+      else:
+        remaining = 0
+        increment = 1
+
+      print "From time %i to %i: %s remaining time: %i" % (quantums,
+      quantums + increment, name, remaining)
+
+      quantums += increment
+      if remaining:
+        owned_processes.append((name, owner, starttime, remaining))
+      else:
+        finished_processes += 1
+        print "Process %s has finished" % name
+        if finished_processes >= len(processes):
+          break
+
+      user_queues.append((owner, owned_processes))
 
 print "Start of STR"
 shortest_time_remaining([("A","Mohamed",1,3), ("B","Mohamed",2,1),
 ("C","Mohamed",3,1), ("D","Mohamed",8,3)])
 
-print "\n\nStart of lottery"
+print "\n\n"
+
+print "Start of lottery"
 processes = [("A","Mohamed",1,10), ("B","Nourhan",2,7), ("C","Mohamed",3,5), ("D","Nourhan",8,3)]
-priorities = [("Mohamed", 1), ("Nourhan", 9)]
+priorities = [("Mohamed", 5), ("Nourhan", 5)]
 print processes
 print priorities
 lottery(processes, priorities)
+
+print "\n\n"
+print "Start of fair share"
+fair_share(processes, priorities)
